5fef42e8a7148d63bf24da8a015acd1f75cfb6f6
HN-TreanT <hntreant23@gmail.com>
2023-04-25 09:05:28 +0700

    update customer page

--- Csharp/src/pages/Home-Page/Center/customer-page/CustomerPage.tsx
+++ Csharp/src/pages/Home-Page/Center/customer-page/CustomerPage.tsx
@@ -1,5 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, Button, Table, Form, Menu, MenuProps, Input } from "antd";
+import {
+  Row,
+  Col,
+  Button,
+  Table,
+  Form,
+  Menu,
+  MenuProps,
+  Input,
+  Modal,
+  Select,
+} from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,14 +20,21 @@ import useAction from "../../../../redux/useActions";
 import { useNavigate } from "react-router-dom";
 import "./CustomerPage.scss";
 import useDebounce from "../../../../hooks/useDebounce";
-
-// import ContentCustomerPage from "./ContenCustomerPage/ContentCustomerPage";
+import { notification } from "../../../../components/notification";
+import { customerServices } from "../../../../untils/networks/services/customerServices";
 interface DataType {
   key: string;
   Fullname: string;
   PhoneNumber: string;
+  Gender: string;
   countOrders: Number;
   TotalPay: Number;
+}
+interface CustomerDetail {
+  Fullname: string;
+  PhoneNumber: string;
+  Gender: string;
+  key: string;
 }
 const items: MenuProps["items"] = [
   {
@@ -24,6 +42,7 @@ const items: MenuProps["items"] = [
     key: "customers",
   },
 ];
+
 const CustomerPage: React.FC = () => {
   const columns: ColumnsType<DataType> = [
     {
@@ -34,6 +53,10 @@ const CustomerPage: React.FC = () => {
     {
       title: "Số điện thoại",
       dataIndex: "PhoneNumber",
+    },
+    {
+      title: "Giới tính",
+      dataIndex: "Gender",
     },
     {
       title: "Số hóa đơn",
@@ -48,7 +71,7 @@ const CustomerPage: React.FC = () => {
       dataIndex: "buttonDelete",
       render: (text: any, record: DataType) => (
         <div
-          onClick={(e) => handleDeleteCategory(e, record)}
+          onClick={(e) => handleDeleteCustomer(e, record)}
           className="table-delete"
           style={{ cursor: "pointer" }}
         >
@@ -57,24 +80,35 @@ const CustomerPage: React.FC = () => {
       ),
     },
   ];
-  const [form] = useForm();
+  const [form] = Form.useForm();
+  const [formAddCustomer] = Form.useForm();
+  const [formEditCustomer] = Form.useForm();
   const actions = useAction();
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   let selectedPage = useSelector((state: any) => state.customer.selectedPage);
+  const [IsOpenModalCreate, setIsOpenModalCreate] = useState(false);
+  const [IsOpenModalEdit, setIsOpenModalEdit] = useState(false);
   const [searchValue, setSearchValue] = useState("");
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [customerDetail, setCustomerDetail] = useState<CustomerDetail>();
   const searchValueDebounce = useDebounce<string>(searchValue, 500);
   useEffect(() => {
     dispatch(actions.CustomerActions.setSearchValue(searchValueDebounce));
     dispatch(actions.CustomerActions.loadData());
-  }, [actions.CustomerActions, dispatch, selectedPage, searchValueDebounce]);
+  }, [
+    actions.CustomerActions,
+    dispatch,
+    selectedPage,
+    searchValueDebounce,
+    customerDetail,
+  ]);
   let customers = useSelector((state: any) => state.customer.customers);
-  console.log(customers);
-  const valueCustomers = customers.Data?.map((customer: any) => {
+  const valueCustomers = customers?.Data?.map((customer: any) => {
     return {
       key: customer?.IdCustomer,
       Fullname: customer?.Fullname,
       PhoneNumber: customer?.PhoneNumber,
+      Gender: customer?.Gender === "nam" ? "Nam" : "Nữ",
       countOrders: customer?.Orders.length,
       TotalPay: customer?.Orders?.reduce(function (
         total: Number,
@@ -96,26 +130,334 @@ const CustomerPage: React.FC = () => {
   });
 
   const loading = useSelector((state: any) => state.state.loadingState);
-  const handleClickButtonAddProduct = () => {};
-  const handleRowClick = (record: any) => {};
+  const handleClickButtonAddProduct = () => {
+    setIsOpenModalCreate(true);
+  };
+  const handleRowClick = (record: any) => {
+    setIsOpenModalEdit(true);
+    setCustomerDetail({
+      Fullname: record.Fullname ? record.Fullname : "",
+      PhoneNumber: record.PhoneNumber ? record.PhoneNumber : "",
+      Gender: record.Gender ? record.Gender : "",
+      key: record.key ? record.key : "",
+    });
+  };
   const handleValueFormChange = () => {};
   const handleSearchValueChange = (e: any) => {
     setSearchValue(e.target.value);
   };
-  const handleDeleteCategory = (e: any, record: any) => {};
+  const handleDeleteCustomer = async (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    record: any
+  ) => {
+    e.stopPropagation();
+    console.log(record.key);
+    try {
+      dispatch(actions.StateAction.loadingState(true));
+      const reponse = await customerServices.deleteCustomer(record.key);
+      if (reponse.Status) {
+        dispatch(actions.CustomerActions.loadData());
+        dispatch(actions.StateAction.loadingState(false));
+        notification({
+          message: "xóa thành công",
+          title: "Thông báo",
+          position: "top-right",
+          type: "success",
+        });
+      } else {
+        notification({
+          message: "xóa không thành công",
+          title: "Thông báo",
+          position: "top-right",
+          type: "danger",
+        });
+        dispatch(actions.StateAction.loadingState(false));
+      }
+    } catch (err: any) {
+      notification({
+        message: "server không trả lời",
+        title: "Thông báo",
+        position: "top-right",
+        type: "danger",
+      });
+      dispatch(actions.StateAction.loadingState(false));
+    }
+  };
+  const handleCreateCustomer = async () => {
+    try {
+      dispatch(actions.StateAction.loadingState(true));
+      let response = await customerServices.createCustomer(
+        formAddCustomer.getFieldsValue()
+      );
+      if (response.Status) {
+        dispatch(actions.CustomerActions.loadData());
+        dispatch(actions.StateAction.loadingState(false));
+        setIsOpenModalCreate(false);
+        formAddCustomer.resetFields();
+        notification({
+          message: "Thêm khách hàng thành công",
+          title: "Thông báo",
+          position: "top-right",
+          type: "success",
+        });
+      } else {
+        dispatch(actions.StateAction.loadingState(false));
+        setIsOpenModalCreate(false);
+        formAddCustomer.resetFields();
+        notification({
+          message: response.Message,
+          title: "Thông báo",
+          position: "top-right",
+          type: "danger",
+        });
+      }
+    } catch (err: any) {
+      setIsOpenModalCreate(false);
+      formAddCustomer.resetFields();
+      notification({
+        message: "server không phản hồi",
+        title: "Thông báo",
+        position: "top-right",
+        type: "danger",
+      });
+    }
+  };
+  const handleValueCreateChange = () => {
+    if (
+      formAddCustomer.getFieldsValue().Fullname &&
+      formAddCustomer.getFieldsValue().phoneNumber &&
+      formAddCustomer.getFieldsValue().gender
+    ) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  };
+  const handleValueEditChange = () => {
+    if (
+      formEditCustomer.getFieldsValue().Fullname &&
+      formEditCustomer.getFieldsValue().phoneNumber &&
+      formEditCustomer.getFieldsValue().gender
+    ) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  };
+  const handleUpdateCustomer = async () => {
+    setIsOpenModalEdit(false);
+    try {
+      if (customerDetail?.key) {
+        dispatch(actions.StateAction.loadingState(true));
+        let response = await customerServices.updateCustomer(
+          customerDetail?.key,
+          formEditCustomer.getFieldsValue()
+        );
+        if (response.Status) {
+          dispatch(actions.CustomerActions.loadData());
+          dispatch(actions.StateAction.loadingState(false));
+          notification({
+            message: "Update success",
+            title: "Thông báo",
+            position: "top-right",
+            type: "success",
+          });
+        } else {
+          notification({
+            message: response.Message,
+            title: "Thông báo",
+            position: "top-right",
+            type: "danger",
+          });
+        }
+      }
+    } catch (err: any) {
+      notification({
+        message: "server không phản hồi",
+        title: "Thông báo",
+        position: "top-right",
+        type: "danger",
+      });
+    }
+  };
   return (
     <div className="customer-page">
+      <Modal
+        title="Thêm khách hàng"
+        open={IsOpenModalCreate}
+        onCancel={() => {
+          setIsOpenModalCreate(false);
+          formAddCustomer.resetFields();
+        }}
+        footer={[
+          <Button
+            key="back"
+            onClick={() => {
+              setIsOpenModalCreate(false);
+              formAddCustomer.resetFields();
+            }}
+          >
+            Hủy
+          </Button>,
+          <Button
+            disabled={isDisabled}
+            key="submit"
+            type="primary"
+            onClick={handleCreateCustomer}
+          >
+            Thêm
+          </Button>,
+        ]}
+      >
+        <Form
+          form={formAddCustomer}
+          layout="vertical"
+          onValuesChange={handleValueCreateChange}
+        >
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: "Tên khách hàng không được bỏ trống",
+              },
+            ]}
+            name="Fullname"
+            label="Tên khách hàng"
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: "Số điện thoại khách hàng không được bỏ trống",
+              },
+            ]}
+            name="phoneNumber"
+            label="Số điện thoại"
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: "Giới tinh khách hàng không được bỏ trống",
+              },
+            ]}
+            name="gender"
+            label="Giới tính"
+          >
+            <Select
+              options={[
+                {
+                  label: "Nam",
+                  value: "nam",
+                },
+                {
+                  label: "Nữ",
+                  value: "Nu",
+                },
+              ]}
+            ></Select>
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Modal
+        title="Chỉnh sửa thông tin khách hàng"
+        open={IsOpenModalEdit}
+        onCancel={() => {
+          setIsOpenModalEdit(false);
+          formAddCustomer.resetFields();
+        }}
+        footer={[
+          <Button
+            key="back"
+            onClick={() => {
+              setIsOpenModalEdit(false);
+              formAddCustomer.resetFields();
+            }}
+          >
+            Hủy
+          </Button>,
+          <Button
+            disabled={isDisabled}
+            key="submit"
+            type="primary"
+            onClick={handleUpdateCustomer}
+          >
+            Lưu thay đổi
+          </Button>,
+        ]}
+      >
+        <Form
+          form={formEditCustomer}
+          layout="vertical"
+          onValuesChange={handleValueEditChange}
+        >
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: "Tên khách hàng không được bỏ trống",
+              },
+            ]}
+            name="Fullname"
+            label="Tên khách hàng"
+            initialValue={customerDetail?.Fullname}
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: "Số điện thoại khách hàng không được bỏ trống",
+              },
+            ]}
+            initialValue={customerDetail?.PhoneNumber}
+            name="phoneNumber"
+            label="Số điện thoại"
+          >
+            <Input></Input>
+          </Form.Item>
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: "Giới tinh khách hàng không được bỏ trống",
+              },
+            ]}
+            name="gender"
+            label="Giới tính"
+            initialValue={customerDetail?.Gender}
+          >
+            <Select
+              options={[
+                {
+                  label: "Nam",
+                  value: "nam",
+                },
+                {
+                  label: "Nữ",
+                  value: "Nu",
+                },
+              ]}
+            ></Select>
+          </Form.Item>
+        </Form>
+      </Modal>
       <Row gutter={[0, 15]}>
         <Col span={24}>
           <div className="title-button-customer-page">
-            <div className="title-customer-page">Danh sách mặt hàng</div>
+            <div className="title-customer-page">Danh sách khách hàng</div>
             <Button
               onClick={handleClickButtonAddProduct}
               type="primary"
               className="button-add-customer"
             >
               <FontAwesomeIcon icon={faPlus} />
-              <span> Thêm mặt hàng</span>
+              <span> Thêm khách hàng</span>
             </Button>
           </div>
         </Col>
